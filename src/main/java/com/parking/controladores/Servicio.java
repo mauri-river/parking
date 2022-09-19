ac4ba6bd8df6bb0560a87ac11971a3ead8b11412
mauri-river <111911758+mauri-river@users.noreply.github.com>
2022-09-19 18:09:24 -0500

    primera version index

---
+++ src/main/java/com/parking/controladores/Servicio.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.parking.controladores;
+
+/**
+ *
+ * @author MAURICIO RIVERA
+ */
+class Servicio {
+
+    static Object consultarCategorias() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Categoria consultarCategorias(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Object consultarCategorias(String Criterio) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void crearNuevaCategoria(Categoria c) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
